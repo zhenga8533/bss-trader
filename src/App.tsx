79b236b9d930221838b3239604f5b316d8004bc7
Allen Zheng <zhenga8533@gmail.com>
2024-07-19 12:19:57 -0400

    Initialized background

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box } from "@chakra-ui/react";
+import background from "./assets/background.jpg";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Box position="relative">
+      <Box
+        minH="100vh"
+        minW="100vw"
+        backgroundImage={background}
+        backgroundSize="cover"
+        filter="blur(24px)"
+        position="absolute"
+        zIndex={0}
+      />
+      <Box
+        backgroundColor="rgba(0, 0, 0, 0.5)"
+        outline="5px solid rgba(255, 255, 0, 0.5)"
+        borderRadius={10}
+        zIndex={10}
+        mt={10}
+        ml="10vw"
+        width="80vw"
+        minH="80vh"
+        display="flex"
+        position="absolute"
+      ></Box>
+    </Box>
+  );
+};
 
-export default App
+export default App;
