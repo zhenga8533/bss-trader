c25ebc8bb902b5f9d0a54b401f99350b63cecadd
Allen Zheng <zhenga8533@gmail.com>
2024-08-02 16:18:18 -0400

    Initialized sticker modal

--- src/components/StickerModal.tsx
+++ src/components/StickerModal.tsx
@@ -1,4 +1,17 @@
-import { Modal, ModalCloseButton, ModalContent, ModalHeader } from "@chakra-ui/react";
+import {
+  Button,
+  Center,
+  Grid,
+  Image,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  Spinner,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import eggs from "../data/eggs.json";
 
 interface StickerModalProps {
   isOpen: boolean;
@@ -6,11 +19,36 @@ interface StickerModalProps {
 }
 
 const StickerModal = ({ isOpen, onClose }: StickerModalProps) => {
+  const [sticker, setSticker] = useState(null);
+  const [spinning, setSpinning] = useState(false);
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalContent>
         <ModalCloseButton />
         <ModalHeader>Sticker Printer</ModalHeader>
+        <ModalBody>
+          <Grid
+            templateColumns="repeat(auto-fill, minmax(90px, 1fr))"
+            bgColor="rgba(0, 0, 0, 0.2)"
+            borderRadius={5}
+            columnGap={3}
+            rowGap={5}
+            className="custom-scroll"
+            maxH="64vh"
+            overflow="auto"
+            p={6}
+          >
+            {Object.entries(eggs).map(([egg, data]) => (
+              <Button>
+                <Image src={data.image_url} alt={egg} />
+              </Button>
+            ))}
+          </Grid>
+          <Center bgColor="rgba(0, 0, 0, 0.2)" borderRadius={5} my={3} w="100%">
+            {spinning ? <Spinner size="xl" /> : sticker}
+          </Center>
+        </ModalBody>
       </ModalContent>
     </Modal>
   );
